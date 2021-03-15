2bd65bca389f32c1a94161eff92f8cc74343772f
Yaritza <yaritzamicaelleb@gmail.com>
2021-03-15 08:20:39 -0300

    Tarefa Milhas para Km

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MilhasparaKm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string medida;
+            double milhas, km;
+
+            Console.Beep();
+            Console.WriteLine("----------");
+            Console.Write("Digite a medida em milhas: ");
+            medida = Console.ReadLine ();
+            milhas = Convert.ToDouble (medida);
+            km = milhas * 1.609;
+
+            Console.Beep();
+            Console.WriteLine("----------");
+            Console.WriteLine($"A medida em km é: {km}");
+        }
+    }
+}
